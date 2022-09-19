b1017484e28ba0a7a9a3fe3093584bc6c435e02c
alexkaasik <70683933+alexkaasik@users.noreply.github.com>
2022-09-19 09:21:24 +0300

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-read -p "add a number: " -ra array <<< "$@"
-#array=('1' '4' '10' '10')
+read -ra array <<< "$@"
 echo ${array[@]}
 read -p "add a number: " -ra input
 array+=(${input[@]})
